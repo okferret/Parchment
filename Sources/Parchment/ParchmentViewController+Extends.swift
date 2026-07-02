94ba6d4dd5cfa1f46efa7d9d49807bec63d53c20
okdev <wanggang3@staff.sina.com.cn>
2026-07-02 10:26:12 +0800

    update => fix #重命名时的选区范围应设定在文件后缀的.前面#

--- Sources/Parchment/ParchmentViewController+Extends.swift
+++ Sources/Parchment/ParchmentViewController+Extends.swift
@@ -8,6 +8,7 @@
 #if canImport(UIKit)
 
 import UIKit
+import Uchardet
 
 extension ParchmentViewController {
     
@@ -19,9 +20,13 @@ extension ParchmentViewController {
         guard fileURL.isFileURL == true else { return false }
         guard ["txt"].contains(fileURL.pathExtension.lowercased()) == true else { return false }
         var isDir: ObjCBool = .init(false)
-        if FileManager.default.fileExists(atPath: fileURL.path, isDirectory: &isDir) == true, isDir.boolValue == false {
+        guard FileManager.default.fileExists(atPath: fileURL.path, isDirectory: &isDir) == true, isDir.boolValue == false else {
+            return false
+        }
+        do {
+            _ = try Uchardet.detect(fileURL).encoding
             return true
-        } else {
+        } catch {
             return false
         }
     }
